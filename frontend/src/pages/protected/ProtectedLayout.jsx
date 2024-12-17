83f4bb2ed4514c13cebb58c34f45b1578bc4ec2a
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-17 17:44:35 +0530

    outlet added

--- frontend/src/pages/protected/ProtectedLayout.jsx
+++ frontend/src/pages/protected/ProtectedLayout.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Stack } from "@mui/material";
+import Header from "../../components/common/Header";
 
 const ProtectedLayout = () => {
-  return <Outlet />;
+  return (
+    <Stack
+      flexDirection={"column"}
+      maxWidth={"800px"}
+      minWidth={"100%"}
+      mx={"auto"}
+      overflow={"hidden"}
+    >
+      <Header />
+      <Outlet />
+    </Stack>
+  );
 };
 
 export default ProtectedLayout;
