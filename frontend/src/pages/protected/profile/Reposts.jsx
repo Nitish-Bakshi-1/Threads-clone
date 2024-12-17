f4384298f9d8f49a06dca9776c4a8454395d0a9f
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-18 01:46:30 +0530

    reposts

--- frontend/src/pages/protected/profile/Reposts.jsx
+++ frontend/src/pages/protected/profile/Reposts.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Stack } from "@mui/material";
+import Post from "../../../components/home/Post";
 
 const Reposts = () => {
-  return <div>Reposts</div>;
+  return (
+    <Stack flexDirection={"column"} gap={2} mb={10} width={"800px"} mx={"auto"}>
+      <Post />
+      <Post />
+    </Stack>
+  );
 };
 
 export default Reposts;
