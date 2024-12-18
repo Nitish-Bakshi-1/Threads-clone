f6a0b5d9cf06ff82f009b9c3b2b38b60c6345d94
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-19 01:25:57 +0530

    navigation

--- frontend/src/components/modals/AddPost.jsx
+++ frontend/src/components/modals/AddPost.jsx
@@ -1,10 +1,74 @@
 import React from "react";
-import { Dialog } from "@mui/material";
+import {
+  Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  useMediaQuery,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { RxCross2 } from "react-icons/rx";
+import { FaImages } from "react-icons/fa";
 
 const AddPost = () => {
+  const _700 = useMediaQuery("(min-width:700px)");
+  const _500 = useMediaQuery("(min-width:500px)");
+  const _300 = useMediaQuery("(min-width:300px)");
+
+  const handleClose = () => {};
+
   return (
     <>
-      <Dialog></Dialog>
+      <Dialog
+        open={true}
+        onClose={handleClose}
+        fullWidth
+        fullScreen={_700 ? false : true}
+      >
+        <Box position={"absolute"} top={20} right={20} onclick={handleClose}>
+          <RxCross2 size={28} className="image-icon" />
+        </Box>
+        <DialogTitle textAlign={"center"} mb={5}>
+          New thread ...
+        </DialogTitle>
+        <DialogContent>
+          <Stack flexDirection={"row"} g={2} mb={5}>
+            <Avatar src="" alt="" />
+            <Stack>
+              <Typography variant="h6" fontWeight={"bold"} fontSize={"1rem"}>
+                NitishBakhi001
+              </Typography>
+              <textarea
+                cols={_500 ? 40 : 25}
+                rows={2}
+                className="text1"
+                placeholder="Start a thread..."
+                autoFocus
+              />
+              <img
+                src=""
+                alt=""
+                id="url-img"
+                width={_500 ? 300 : _300 ? 200 : 100}
+                height={_500 ? 300 : _300 ? 200 : 100}
+              />
+              <FaImages size={28} className="image-icon" />
+              <input type="file" accept="image/*" className="file-input" />
+            </Stack>
+          </Stack>
+          <Stack
+            flexDirection={"row"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <Typography variant="h6" fontSize={"1rem"} color={"gray"}>
+              Anyone can reply...
+            </Typography>
+            <Button></Button>
+          </Stack>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
