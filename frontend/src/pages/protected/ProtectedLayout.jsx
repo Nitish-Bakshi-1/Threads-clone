9791f0c126e14db956f73bce7ae8a889d84bdc87
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-19 03:54:13 +0530

    init

--- frontend/src/pages/protected/ProtectedLayout.jsx
+++ frontend/src/pages/protected/ProtectedLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import { Stack, useMediaQuery } from "@mui/material";
 import Header from "../../components/common/Header";
 import AddPost from "../../components/modals/AddPost";
+import EditProfile from "../../components/modals/EditProfile";
 
 const ProtectedLayout = () => {
   const _700 = useMediaQuery("(min-width:700px)");
@@ -16,7 +17,8 @@ const ProtectedLayout = () => {
       overflow={"hidden"}
     >
       <Header />
-      <AddPost />
+      {/* <AddPost /> */}
+      <EditProfile />
       <Outlet />
     </Stack>
   );
