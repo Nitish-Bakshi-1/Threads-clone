46ba974a240b0424b06fdc5b2a0119e6e62a8d8b
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-19 03:32:34 +0530

    init

--- frontend/src/components/modals/EditProfile.jsx
+++ frontend/src/components/modals/EditProfile.jsx
@@ -1,12 +1,72 @@
 import React from "react";
-import { Dialog } from "@mui/material";
+import {
+  Dialog,
+  useMediaQuery,
+  Box,
+  DialogTitle,
+  DialogContent,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { RxCross2 } from "react-icons/rx";
 
 const EditProfile = () => {
+  const _700 = useMediaQuery("(min-width:700px)");
   const handleClose = () => {};
   return (
     <>
-      <Dialog open={true} onClose={handleClose}>
-        {" "}
+      <Dialog
+        open={true}
+        onClose={handleClose}
+        fullWidth
+        fullScreen={_700 ? false : true}
+      >
+        <Box position={"absolute"} top={20} right={20} onClick={handleClose}>
+          <RxCross2 size={28} />
+        </Box>
+        <DialogTitle textAlign={"center"} mb={5}>
+          Edit profile
+        </DialogTitle>
+        <DialogContent>
+          <Stack flexDirection={"column"} gap={1}>
+            <Avatar
+              src=""
+              alt=""
+              sx={{ height: 96, height: 96, alignSelf: "center" }}
+            />
+            <Button
+              size={"large"}
+              sx={{
+                border: "2px solid grey",
+                borderRadius: "10px",
+                width: 96,
+                height: 40,
+                alignSelf: "center",
+                my: 2,
+                ":hover": {
+                  cursor: "pointer",
+                },
+              }}
+            >
+              Change
+            </Button>
+            <input type="file" className="file-input" accept={"image/*"} />
+            <Typography
+              variant="subtitle1"
+              fontWeight={"bold"}
+              fontSize={"1.2rem"}
+              my={2}
+            >
+              username
+            </Typography>
+            <input
+              type="text"
+              value={"nitishbakshi"}
+              readOnly
+              className="text1"
+            />
+          </Stack>
+        </DialogContent>
       </Dialog>
     </>
   );
