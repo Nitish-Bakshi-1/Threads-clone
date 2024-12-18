a8e62dc2e5b5e726b3910680e75de84e4f68ecb7
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-19 01:00:14 +0530

    navigation

---
+++ frontend/src/components/modals/AddPost.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Dialog } from "@mui/material";
+
+const AddPost = () => {
+  return (
+    <>
+      <Dialog></Dialog>
+    </>
+  );
+};
+
+export default AddPost;
