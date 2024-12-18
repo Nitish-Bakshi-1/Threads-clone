c4abf2a303e39ce61c322afa5801d08e903e8130
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-19 03:10:56 +0530

    init

---
+++ frontend/src/components/modals/EditProfile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Dialog } from "@mui/material";
+
+const EditProfile = () => {
+  const handleClose = () => {};
+  return (
+    <>
+      <Dialog open={true} onClose={handleClose}>
+        {" "}
+      </Dialog>
+    </>
+  );
+};
+
+export default EditProfile;
