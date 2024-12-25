b79ddfe928c398a5cb22399bd86183f7dba97126
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-25 05:52:51 +0530

    init

---
+++ backend/models/comment-model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const commentSchema = new Mongoose.Schema(
+  {
+    admin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+    text: {
+      type: String,
+    },
+  },
+  { timeStamps: true }
+);
+
+const Comment = mongoose.model("comment", commentSchema);
+export default Comment;
---
+++ backend/models/post-model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    admin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+    text: {
+      type: String,
+    },
+    media: {
+      type: String,
+    },
+    public_id: {
+      type: String,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  { timeStamps: true }
+);
+
+const Post = mongoose.model("post", postSchema);
+export default Post;
